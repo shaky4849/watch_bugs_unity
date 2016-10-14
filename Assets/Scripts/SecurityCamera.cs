a5d6b967d00d2ffdcfffe450c22f9047b212a0e1
lucasanizelli <lucasanizelli@gmail.com>
2016-10-14 19:34:42 -0300

    Camera jump
    Signed-off-by: lucasanizelli <lucasanizelli@gmail.com>

---
+++ Assets/Scripts/SecurityCamera.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecurityCamera : MonoBehaviour 
+{
+	public Transform cameraObject;
+
+	public int raycastDistance;
+	public int horizontalSensivity;
+	public int verticalSensivity;
+
+	public LayerMask raycastLayer;
+
+	public bool isEnabled = false;
+
+	RaycastHit hitInfo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (isEnabled) 
+		{
+			cameraObject.Rotate (Input.GetAxis ("Mouse Y") * Time.deltaTime * horizontalSensivity, 0, 0);
+			cameraObject.Rotate (0, Input.GetAxis ("Mouse X") * Time.deltaTime * verticalSensivity * -1f, 0, Space.World);
+
+			if (Physics.Raycast (cameraObject.position, cameraObject.forward, out hitInfo, raycastDistance, raycastLayer)) 
+			{
+				UIAcessMessage.Instance.ShowMessage (hitInfo.transform.position, "Hack");
+
+				if (Input.GetKeyDown (KeyCode.E)) 
+				{
+					Debug.Log ("KeyPressed");
+					Debug.Log ("Sending To: " + hitInfo.transform.gameObject.name);
+					isEnabled = false;
+					UIAcessMessage.Instance.CleanMessage ();
+					Transform cameraNewPosition = hitInfo.transform.FindChild("CameraPosition");
+					Camera.main.transform.parent = cameraNewPosition;
+					LeanTween.move (Camera.main.gameObject, cameraNewPosition.position, .8f).setOnComplete(OnCompleteAnim);
+					LeanTween.rotate (Camera.main.gameObject, cameraNewPosition.rotation.eulerAngles, .8f);
+				}
+			}
+			else
+				UIAcessMessage.Instance.CleanMessage ();
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+
+	}
+
+	void OnCompleteAnim()
+	{
+		hitInfo.transform.GetComponent<SecurityCamera> ().isEnabled = true;
+		//hitInfo.transform.GetComponent<SphereCollider> ().radius = 0;
+		//GetComponent<SphereCollider> ().radius = 2;
+	}
+}
---
+++ Assets/Scripts/UIAcessMessage.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIAcessMessage : MonoBehaviour
+{
+	Text textMessage;
+	RectTransform textRectTransform;
+
+	public static UIAcessMessage _instance;
+	public static UIAcessMessage Instance { get { return _instance; } }
+
+	void Awake()
+	{
+		if (_instance != null && _instance != this)
+			Destroy (this.gameObject);
+		else
+			_instance = this;
+
+		textMessage = gameObject.GetComponent<Text> ();
+		textRectTransform = gameObject.GetComponent<RectTransform> ();
+	}
+
+	public void ShowMessage(Vector3 messagePosition, string message)
+	{
+		Vector2 uiNewPosition = GetCanvasPosition (messagePosition);
+		uiNewPosition.x += 65f;
+		textRectTransform.anchoredPosition = uiNewPosition;
+		textMessage.text = message;
+	}
+
+	public void CleanMessage()
+	{
+		textMessage.text = "";
+	}
+
+	Vector2 GetCanvasPosition(Vector3 worldObjectPoint)
+	{
+		//first you need the RectTransform component of your canvas
+		RectTransform CanvasRect = transform.parent.GetComponent<RectTransform> ();
+
+		//then you calculate the position of the UI element
+		//0,0 for the canvas is at the center of the screen, whereas WorldToViewPortPoint treats the lower left corner as 0,0. Because of this, you need to subtract the height / width of the canvas * 0.5 to get the correct position.
+		Vector2 ViewportPosition = Camera.main.WorldToViewportPoint (worldObjectPoint);
+		Vector2 WorldObject_ScreenPosition = new Vector2 (
+			                                    ((ViewportPosition.x * CanvasRect.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f)),
+			                                    ((ViewportPosition.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)));
+
+		//now you can set the position of the ui element
+		return WorldObject_ScreenPosition;
+	}
+}
